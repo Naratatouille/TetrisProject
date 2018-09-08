5d88f66c505a1dfe7e0ced9c7a083a451d6c1669
Dorrmeur <33933325+Dorrmeur@users.noreply.github.com>
2018-09-08 18:23:09 +0200

    Add files via upload

---
+++ Bloc.cpp
@@ -0,0 +1,85 @@
+#include "Bloc.h"
+
+Bloc::Bloc() :
+	width(30), height(30), color(1), index(1), xposition(1), yposition(1)
+{
+	std::cout << "constucteur bloc" << std::endl;
+}
+
+Bloc::Bloc(int _X, int _Y, int _col) :
+	width(30), height(30), color(_col), index(1), xposition(_X), yposition(_Y)
+{
+	std::cout << "constucteur value" << std::endl;
+}
+
+Bloc::Bloc(const Bloc& _bloc) :
+	width(_bloc.width), height(_bloc.height), color(_bloc.color), index(_bloc.index), xposition(_bloc.xposition), yposition(_bloc.yposition)
+{
+	std::cout << "constucteur recopie" << std::endl;
+}
+
+Bloc::~Bloc()
+{
+	std::cout << "destructeur" << std::endl;
+}
+
+
+int Bloc::getWidth()
+{
+	return width;
+}
+
+int Bloc::getHeight()
+{
+	return height;
+}
+
+int Bloc::getColor()
+{
+	return color;
+}
+
+int Bloc::getXposition()
+{
+	return xposition;
+}
+
+int Bloc::getYposition()
+{
+	return yposition;
+}
+
+void Bloc::BlockDisp() const
+{
+	const int SCREEN_WIDTH = 640;
+	const int SCREEN_HEIGHT = 480;
+	SDL_Window* gWindow = NULL;
+	SDL_Surface* gScreenSurface = NULL;
+	SDL_Surface* gHelloWorld = NULL;
+
+	//Create window
+	gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	//Get window surface
+	gScreenSurface = SDL_GetWindowSurface(gWindow);
+	//Load splash image
+	gHelloWorld = SDL_LoadBMP("Carree_Vert.bmp");
+	//Apply the image
+	SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+	//Update the surface
+	SDL_UpdateWindowSurface(gWindow);
+	//Wait five seconds
+	SDL_Delay(5000);
+}
+
+void Test_Bloc()
+{
+	Bloc bloc1;
+	Bloc bloc2(15, 25, 4);
+	Bloc bloc3(bloc2);
+	std::cout << "Couleur bloc 1 : " << bloc1.getColor() << std::endl;
+	std::cout << "Largeur bloc 1 : " << bloc1.getWidth() << std::endl;
+	std::cout << "Hauteur bloc 1 : " << bloc1.getHeight() << std::endl;
+	std::cout << "Hauteur bloc 2 : " << bloc2.getYposition() << std::endl;
+	std::cout << "Hauteur bloc 3 : " << bloc3.getXposition() << std::endl;
+	bloc1.BlockDisp();
+}
---
+++ Bloc.h
@@ -0,0 +1,38 @@
+#ifndef BLOC_H
+#define BLOC_H
+
+#include <SDL.h>
+#include <stdio.h>
+#include <iostream>
+
+class Bloc
+{
+public:
+	// Constructeurs 
+
+	Bloc();
+	Bloc(int _X, int _Y, int _col);
+	Bloc(const Bloc&);
+	~Bloc();
+
+	// Get des propriétés de la classe
+
+	int getWidth();
+	int getHeight();
+	int getColor();
+	int getXposition();
+	int getYposition();
+
+	void BlockDisp() const;					// Affichage d'un bloc
+
+private:
+	int width;								// Largeur d'un bloc
+	int height;								// Hauteur 
+	int color;								// Couleur 
+	int xposition;							// Position x
+	int yposition;							// position y 
+	int index;								// Servira a l'indexation
+};
+
+void Test_Bloc();
+#endif
