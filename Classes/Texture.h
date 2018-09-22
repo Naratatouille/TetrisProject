b97437b3dfe9723ba75148d9e620614837116813
Naratatouille <43034276+Naratatouille@users.noreply.github.com>
2018-09-22 14:33:20 +0200

    Add files via upload

---
+++ Classes/Texture.h
@@ -0,0 +1,44 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+#include <ctime>
+#include <cmath>
+#include <vector>
+#include <exception>
+#include <stdexcept>
+#include <stdio.h>
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include "SDL2-2.0.8/include/SDL.h"
+#include "SDL2-2.0.8/include/SDL_ttf.h"
+#include "SDL2-2.0.8/include/SDL_image.h"
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+class Texturer
+{
+	public:
+		Texturer();
+		~Texturer();
+		bool loadfromfile(std::string path);
+		void free();
+		void render(int x, int y);
+
+		//Gets image dimensions
+		int getWidth();
+		int getHeight();
+		static void playgame();
+	
+	private:
+		//The actual hardware texture
+		SDL_Texture * mTexture;
+
+		//Image dimensions
+		int ImWidth;
+		int ImHeight;
+
+};
+
+
+#endif
