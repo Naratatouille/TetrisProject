53f243cb4c67f3b759651bfc85c9b06b38f5b0f4
Flammekueche <hugo.dorr@gmail.com>
2018-10-15 20:34:01 +0200

    ajout init toutes pieces
    initialisation des 7 formes
    (btw, on a enlevé les bloc (h et cpp) dans une version précédente car inutiles

--- Tetris2.1/Tetris/Tetromino.cpp
+++ Tetris2.1/Tetris/Tetromino.cpp
@@ -3,7 +3,7 @@
 
 
 Tetromino::Tetromino() :
-	/*A(), B(), C(), D(),*/ type(I), color(0)
+	/*A(), B(), C(), D(),*/ type(S), color(0)
 {
 	std::cout << "Constructeur Tetromino" << std::endl;
 }
@@ -24,18 +24,19 @@ Tetromino::~Tetromino()
 	std::cout << "destructeur Tetromino" << std::endl;
 }
 
-void Tetromino::initTetro(StructVar& sv)
+void Tetromino::initTetro(StructVar& sv)				// Initialisation des différentes pièces
 {
 	carreau init;
+	for (unsigned int i = 0; i < 4; i++) 
+	{
+			forme.push_back(init);
+	}
 	switch (type) 
 	{
 	case I:
-		for (unsigned int i = 0; i < 4; i++) {
-			forme.push_back(init);
-		}
 		forme[0].xpos = sv.grille[LARGEUR_GRILLE / 2][0].xpos;
 		forme[0].ypos = sv.grille[LARGEUR_GRILLE / 2][0].ypos;
-		sv.grille[LARGEUR_GRILLE / 2][0].bloc = 2;
+		sv.grille[LARGEUR_GRILLE / 2][0].bloc = 2;								// Ligne plus importante pour affichage
 		forme[1].xpos = sv.grille[LARGEUR_GRILLE / 2][1].xpos;
 		forme[1].ypos = sv.grille[LARGEUR_GRILLE / 2][1].ypos;
 		sv.grille[LARGEUR_GRILLE / 2][1].bloc = 2;
@@ -45,7 +46,97 @@ void Tetromino::initTetro(StructVar& sv)
 		forme[3].xpos = sv.grille[LARGEUR_GRILLE / 2][3].xpos;
 		forme[3].ypos = sv.grille[LARGEUR_GRILLE / 2][3].ypos;
 		sv.grille[LARGEUR_GRILLE / 2][3].bloc = 2;
-		std::cout << "test" << std::endl;
+		break;
+
+	case O:
+		forme[0].xpos = sv.grille[LARGEUR_GRILLE / 2][0].xpos;
+		forme[0].ypos = sv.grille[LARGEUR_GRILLE / 2][0].ypos;
+		sv.grille[LARGEUR_GRILLE / 2][0].bloc = 2;
+		forme[1].xpos = sv.grille[LARGEUR_GRILLE / 2][1].xpos;
+		forme[1].ypos = sv.grille[LARGEUR_GRILLE / 2][1].ypos;
+		sv.grille[LARGEUR_GRILLE / 2][1].bloc = 2;
+		forme[2].xpos = sv.grille[LARGEUR_GRILLE / 2 - 1][0].xpos;
+		forme[2].ypos = sv.grille[LARGEUR_GRILLE / 2 - 1][0].ypos;
+		sv.grille[LARGEUR_GRILLE / 2 - 1][0].bloc = 2;
+		forme[3].xpos = sv.grille[LARGEUR_GRILLE / 2 - 1][1].xpos;
+		forme[3].ypos = sv.grille[LARGEUR_GRILLE / 2 - 1][1].ypos;
+		sv.grille[LARGEUR_GRILLE / 2 - 1][1].bloc = 2;
+		break;
+
+	case T:
+		forme[0].xpos = sv.grille[LARGEUR_GRILLE / 2][0].xpos;
+		forme[0].ypos = sv.grille[LARGEUR_GRILLE / 2][0].ypos;
+		sv.grille[LARGEUR_GRILLE / 2][0].bloc = 2;
+		forme[1].xpos = sv.grille[LARGEUR_GRILLE / 2][1].xpos;
+		forme[1].ypos = sv.grille[LARGEUR_GRILLE / 2][1].ypos;
+		sv.grille[LARGEUR_GRILLE / 2][1].bloc = 2;
+		forme[2].xpos = sv.grille[LARGEUR_GRILLE / 2][2].xpos;
+		forme[2].ypos = sv.grille[LARGEUR_GRILLE / 2][2].ypos;
+		sv.grille[LARGEUR_GRILLE / 2][2].bloc = 2;
+		forme[3].xpos = sv.grille[LARGEUR_GRILLE / 2 - 1][1].xpos;
+		forme[3].ypos = sv.grille[LARGEUR_GRILLE / 2 - 1][1].ypos;
+		sv.grille[LARGEUR_GRILLE / 2 - 1][1].bloc = 2;
+		break;
+
+	case L:
+		forme[0].xpos = sv.grille[LARGEUR_GRILLE / 2 - 1][0].xpos;
+		forme[0].ypos = sv.grille[LARGEUR_GRILLE / 2 -1][0].ypos;
+		sv.grille[LARGEUR_GRILLE / 2 - 1][0].bloc = 2;
+		forme[1].xpos = sv.grille[LARGEUR_GRILLE / 2 - 1][1].xpos;
+		forme[1].ypos = sv.grille[LARGEUR_GRILLE / 2 - 1][1].ypos;
+		sv.grille[LARGEUR_GRILLE / 2 - 1][1].bloc = 2;
+		forme[2].xpos = sv.grille[LARGEUR_GRILLE / 2][2].xpos;
+		forme[2].ypos = sv.grille[LARGEUR_GRILLE / 2][2].ypos;
+		sv.grille[LARGEUR_GRILLE / 2 - 1][2].bloc = 2;
+		forme[3].xpos = sv.grille[LARGEUR_GRILLE / 2][2].xpos;
+		forme[3].ypos = sv.grille[LARGEUR_GRILLE / 2][2].ypos;
+		sv.grille[LARGEUR_GRILLE / 2][2].bloc = 2;
+		break;
+
+	case J:
+		forme[0].xpos = sv.grille[LARGEUR_GRILLE / 2][0].xpos;
+		forme[0].ypos = sv.grille[LARGEUR_GRILLE / 2][0].ypos;
+		sv.grille[LARGEUR_GRILLE / 2][0].bloc = 2;
+		forme[1].xpos = sv.grille[LARGEUR_GRILLE / 2][1].xpos;
+		forme[1].ypos = sv.grille[LARGEUR_GRILLE / 2][1].ypos;
+		sv.grille[LARGEUR_GRILLE / 2][1].bloc = 2;
+		forme[2].xpos = sv.grille[LARGEUR_GRILLE / 2][2].xpos;
+		forme[2].ypos = sv.grille[LARGEUR_GRILLE / 2][2].ypos;
+		sv.grille[LARGEUR_GRILLE / 2][2].bloc = 2;
+		forme[3].xpos = sv.grille[LARGEUR_GRILLE / 2 -  1][2].xpos;
+		forme[3].ypos = sv.grille[LARGEUR_GRILLE / 2 - 1][2].ypos;
+		sv.grille[LARGEUR_GRILLE / 2 -  1][2].bloc = 2;
+		break;
+
+	case Z:
+		forme[0].xpos = sv.grille[LARGEUR_GRILLE / 2][0].xpos;
+		forme[0].ypos = sv.grille[LARGEUR_GRILLE / 2][0].ypos;
+		sv.grille[LARGEUR_GRILLE / 2][0].bloc = 2;
+		forme[1].xpos = sv.grille[LARGEUR_GRILLE / 2 - 1][0].xpos;
+		forme[1].ypos = sv.grille[LARGEUR_GRILLE / 2 - 1][0].ypos;
+		sv.grille[LARGEUR_GRILLE / 2 - 1][0].bloc = 2;
+		forme[2].xpos = sv.grille[LARGEUR_GRILLE / 2][1].xpos;
+		forme[2].ypos = sv.grille[LARGEUR_GRILLE / 2][1].ypos;
+		sv.grille[LARGEUR_GRILLE / 2][1].bloc = 2;
+		forme[3].xpos = sv.grille[LARGEUR_GRILLE / 2 + 1][1].xpos;
+		forme[3].ypos = sv.grille[LARGEUR_GRILLE / 2 + 1][1].ypos;
+		sv.grille[LARGEUR_GRILLE / 2 + 1][1].bloc = 2;
+		break;
+
+	case S:
+		forme[0].xpos = sv.grille[LARGEUR_GRILLE / 2][0].xpos;
+		forme[0].ypos = sv.grille[LARGEUR_GRILLE / 2][0].ypos;
+		sv.grille[LARGEUR_GRILLE / 2][0].bloc = 2;
+		forme[1].xpos = sv.grille[LARGEUR_GRILLE / 2 + 1][0].xpos;
+		forme[1].ypos = sv.grille[LARGEUR_GRILLE / 2 + 1][0].ypos;
+		sv.grille[LARGEUR_GRILLE / 2 + 1][0].bloc = 2;
+		forme[2].xpos = sv.grille[LARGEUR_GRILLE / 2][1].xpos;
+		forme[2].ypos = sv.grille[LARGEUR_GRILLE / 2][1].ypos;
+		sv.grille[LARGEUR_GRILLE / 2][1].bloc = 2;
+		forme[3].xpos = sv.grille[LARGEUR_GRILLE / 2 - 1][1].xpos;
+		forme[3].ypos = sv.grille[LARGEUR_GRILLE / 2 - 1][1].ypos;
+		sv.grille[LARGEUR_GRILLE / 2 - 1][1].bloc = 2;
+		break;
 	}
 
 }
